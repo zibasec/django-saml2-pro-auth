9ad07db4559bd4c71ab9aae25df5c7eb1fcca81c
HeavenSleep <majyx.software@gmail.com>
2018-08-08 15:00:23 +0200

    add default attribute value when strict is disabled (#25)
    * add default attribute value when strict is disabled
    
    * update documentation
    
    * add exception when using default value
    while SAML_USERS_STRICT_MAPPING is TRUE
    
    * add test coverage

--- src/django_saml2_pro_auth/auth.py
+++ src/django_saml2_pro_auth/auth.py
@@ -38,7 +38,9 @@ def get_clean_map(user_map, saml_data):
     for usr_k, usr_v in iteritems(user_map):
         if strict_mapping:
             if type(usr_v) is dict:
-                if 'index' in usr_v:
+                if 'default' in usr_v.keys():
+                    raise SAMLSettingsError('A default value is set for key %s in SAML_USER_MAP while SAML_USERS_STRICT_MAPPING is activated' % usr_k)
+                if 'index' in usr_v.keys():
                     final_map[usr_k] = saml_data[usr_v['key']][usr_v['index']]
                 else:
                     final_map[usr_k] = saml_data[usr_v['key']]
@@ -47,9 +49,9 @@ def get_clean_map(user_map, saml_data):
         else:
             if type(usr_v) is dict:
                 if 'index' in usr_v:
-                    final_map[usr_k] = saml_data[usr_v['key']][usr_v['index']] if usr_v['key'] in saml_data else None
+                    final_map[usr_k] = saml_data[usr_v['key']][usr_v['index']] if usr_v['key'] in saml_data else usr_v['default'] if 'default' in usr_v.keys() else None
                 else:
-                    final_map[usr_k] = saml_data[usr_v['key']] if usr_v['key'] in saml_data else None
+                    final_map[usr_k] = saml_data[usr_v['key']] if usr_v['key'] in saml_data else usr_v['default'] if 'default' in usr_v.keys() else None
             else:
                 final_map[usr_k] = saml_data[user_map[usr_k]] if user_map[usr_k] in saml_data else None
 
