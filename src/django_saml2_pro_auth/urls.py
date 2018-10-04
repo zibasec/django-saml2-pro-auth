a2693ae93bea824fe5a375551c9c658702f2605b
Piotr Bilski <piotrbilski@gmail.com>
2018-10-04 15:28:38 +0200

    compatibility with django 2.1.x (#32)
    * updated imports in url.py to use module with django 2.x

--- src/django_saml2_pro_auth/urls.py
+++ src/django_saml2_pro_auth/urls.py
@@ -1,6 +1,9 @@
 import re
-
-from django.conf.urls import url
+try:
+    from django.urls import re_path as url
+except ImportError:
+    from django.conf.urls import url
+    
 from django.conf import settings
 
 from . import views
