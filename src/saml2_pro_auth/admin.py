ffde94deebc38475a7bcc56238e6a1082d38c38e
Daniel Shepherd <daniels@zibasec.io>
2020-11-10 14:15:11 -0500

    Add id field to admin form

--- src/saml2_pro_auth/admin.py
+++ src/saml2_pro_auth/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import SamlProvider
 
-admin.site.register(SamlProvider)
+
+@admin.register(SamlProvider)
+class SamlProviderAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
